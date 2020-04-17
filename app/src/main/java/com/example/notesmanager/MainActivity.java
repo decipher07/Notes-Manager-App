85bf20470941f65ab1f1e87fa7a2e132f927e483
decipher07 <social.deej@gmail.com>
2020-04-18 04:53:38 +0530

    Final Build

--- app/src/main/java/com/example/notesmanager/MainActivity.java
+++ app/src/main/java/com/example/notesmanager/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.notesmanager;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -14,21 +18,33 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 
 public class MainActivity extends AppCompatActivity {
 
     ListView listView ;
     static ArrayAdapter arrayAdapter;
     static ArrayList<String> notes = new ArrayList<String>();
+    SharedPreferences sharedPreferences  ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         listView  = (ListView) findViewById(R.id.listView);
-        notes.add("Example Notes");
+        sharedPreferences = getApplicationContext().getSharedPreferences("com.example.notesmanager", Context.MODE_PRIVATE);
+
+        HashSet<String> set = (HashSet<String>) sharedPreferences.getStringSet("notes", null);
+
+        if (set == null)
+            notes.add("Example Note");
+        else
+            notes = new ArrayList(set);
+
         arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, notes);
         listView.setAdapter(arrayAdapter);
+        final SharedPreferences sharedPreferences = getApplicationContext().getSharedPreferences("com.example.notesmanager", Context.MODE_PRIVATE);
+
 
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
@@ -36,6 +52,32 @@ public class MainActivity extends AppCompatActivity {
                 Intent intent = new Intent(getApplicationContext(), NoteEditor.class);
                 intent.putExtra("noteID", position);
                 startActivity(intent);
+            }
+        });
+
+        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, final int position, long id) {
+
+                new AlertDialog.Builder(MainActivity.this)
+                        .setIcon(android.R.drawable.ic_dialog_alert)
+                        .setTitle("Are You Sure?")
+                        .setMessage("Do You Want to Delete")
+                        .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                 notes.remove(position);
+                                 arrayAdapter.notifyDataSetChanged();
+
+
+                                HashSet<String> set = new HashSet<>(MainActivity.notes);
+                                sharedPreferences.edit().putStringSet("notes", set).apply();
+
+                            }
+                        })
+                        .setNegativeButton("No", null)
+                        .show();
+                return true ;
             }
         });
     }
